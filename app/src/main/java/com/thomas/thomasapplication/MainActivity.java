38828a3d0fd56aeda784023e843e82668d30dcc4
Cyclotronian <aviral.2815@gmail.com>
2018-12-14 11:49:06 -0500

    dataset updated

--- app/src/main/java/com/thomas/thomasapplication/MainActivity.java
+++ app/src/main/java/com/thomas/thomasapplication/MainActivity.java
@@ -162,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String fileNameStoreLabel= "/data/data/"+applicationName+"/directoryTest/mem2Character2ManifestMapFileLabel2.raw";
                 String fileNameStoreNormalization="/data/data/"+applicationName+"/directoryTest/normalizationTranfer.txt";
 
-                prepareFiles("/data/data/"+applicationName+"/", fileNameStoreData,fileNameStoreLabel, fileNameStoreNormalization,"/mnt/sdcard/Download/manifest_mnist_train.txt",60000, 1);
+                prepareFiles("/data/data/"+applicationName+"/", fileNameStoreData,fileNameStoreLabel, fileNameStoreNormalization,"/mnt/sdcard/Download/manifest_cifar_train.txt",60000, 1);
 
             }
         };
@@ -182,8 +182,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 String filename_label="/data/data/"+applicationName+"/directoryTest/mem2Character2ManifestMapFileLabel2.raw";
                 String filename_data="/data/data/"+applicationName+"/directoryTest/mem2Character2ManifestMapFileData2.raw";
-                int imageSize=28;
-                int numOfChannel=1;//black and white => 1; color =>3
+                int imageSize=32;
+                int numOfChannel=3;//black and white => 1; color =>3
                 String storeweightsfile="/data/data/"+applicationName+"/directoryTest/weightstTransferedTEST.dat";
                 String loadweightsfile="/mnt/sdcard/Download/preloadingData/weightstface1.dat";
                 String loadnormalizationfile="/data/data/"+applicationName+"/directoryTest/normalizationTranfer.txt";
@@ -223,7 +223,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Runnable runnable = new Runnable() {
             public void run() {
                 String appDirctory ="/data/data/"+applicationName+"/";
-                String cmdString ="./predict weightsfile=/data/data/"+applicationName+"/directoryTest/weightstTransferedTEST.dat  inputfile=/mnt/sdcard/Download/manifest_mnist_test.txt outputfile=/data/data/"+applicationName+"/directoryTest/pred.txt batchsize=32";
+                String cmdString ="./predict weightsfile=/data/data/"+applicationName+"/directoryTest/weightstTransferedTEST.dat  inputfile=/mnt/sdcard/Download/manifest_cifar_test.txt outputfile=/data/data/"+applicationName+"/directoryTest/pred.txt batchsize=32";
                 prediction(appDirctory,cmdString);
 
             }
